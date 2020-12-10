55dd1cfb9f4a822657ce01e397da262ed4100341
SKaralius <k.simonas.k@gmail.com>
2020-12-10 12:43:49 +0200

    Player stat display.

--- Assets/Scripts/Inventory/EquippedItemsUI.cs
+++ Assets/Scripts/Inventory/EquippedItemsUI.cs
@@ -1,15 +1,21 @@
 ﻿using Unit;
 using UnityEngine;
+using TMPro;
 
 namespace Inventory
 {
     public class EquippedItemsUI : PanelUI
     {
+        [SerializeField] private TextMeshProUGUI statDisplay;
+        private GameObject player;
+        private UnitStats unitStats;
         private EquippedItems eqItems;
 
         private void Awake()
         {
-            eqItems = GameObject.FindGameObjectWithTag("Player").GetComponent<EquippedItems>();
+            player = GameObject.FindGameObjectWithTag("Player");
+            eqItems = player.GetComponent<EquippedItems>();
+            unitStats = player.GetComponent<UnitStats>();
             EventManager.OnItemEquipped += UpdateUI;
             EventManager.OnItemUnequipped += UpdateUI;
             CreateSlot(5);
@@ -18,6 +24,17 @@ namespace Inventory
         protected void UpdateUI(EquippableItem item, int who)
         {
             itemSlots[(int)item.Slot].RenderUI(eqItems.equipedItems[(int)item.Slot]);
+            RenderPlayerStats();
+        }
+
+        private void RenderPlayerStats()
+        {
+            statDisplay.text = "";
+            foreach (EStats stat in System.Enum.GetValues(typeof(EStats)))
+            {
+                statDisplay.text += unitStats.GetStat(stat).ToString();
+                statDisplay.text += "\n";
+            }
         }
 
         private void OnDestroy()
