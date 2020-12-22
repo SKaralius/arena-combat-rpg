5eb94a2edadf34d6a95f464d57a19a00a2d2214d
SKaralius <k.simonas.k@gmail.com>
2020-12-22 11:40:52 +0200

    Move shop button.

---
+++ Assets/Scripts/Inventory/ShopButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Inventory
+{
+    public class ShopButton : MonoBehaviour
+    {
+        private GameObject shopContainer;
+        private Button shopButton;
+
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            shopButton = GetComponent<Button>();
+        }
+
+        private void Start()
+        {
+            shopButton.onClick.AddListener(ToggleShop);
+        }
+
+        private void ToggleShop()
+        {
+            shopContainer = GameObject.Find("Shop");
+            GameObject shopPanelGO = shopContainer.transform.Find("ShopPanel").gameObject;
+            shopPanelGO.GetComponent<ShopUI>().TogglePanel();
+        }
+    }
+}
